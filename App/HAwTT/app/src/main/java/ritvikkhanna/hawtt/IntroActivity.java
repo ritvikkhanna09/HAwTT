73349aa9b395f6f617337e6ddb4f15fdb113513c
Ritvik Khanna <ritvikkhanna09@gmail.com>
2017-08-06 23:39:21 +0530

    Adding Intent

--- App/HAwTT/app/src/main/java/ritvikkhanna/hawtt/IntroActivity.java
+++ App/HAwTT/app/src/main/java/ritvikkhanna/hawtt/IntroActivity.java
@@ -5,18 +5,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class IntroActivity extends AppCompatActivity {
     Button b;
+    EditText edt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
         b=(Button)findViewById(R.id.button_gs);
-        b.setOnClickListener(new View.OnClickListener() {
+        edt=(EditText)findViewById(R.id.edit_field);
+        b.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent I= new Intent(IntroActivity.this,MainActivity.class);
+                I.putExtra("user_name",edt.getText().toString());
                 startActivity(I);
                 finish();
             }
--- App/HAwTT/app/src/main/java/ritvikkhanna/hawtt/MainActivity.java
+++ App/HAwTT/app/src/main/java/ritvikkhanna/hawtt/MainActivity.java
@@ -1,13 +1,39 @@
 package ritvikkhanna.hawtt;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
+    String user_name,greeting_msg;
+    TextView greeting_textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        greeting_textView=(TextView)findViewById(R.id.greeting_text);
+        Intent i=getIntent();
+        user_name= i.getStringExtra("user_name");
+        greeting_msg=getGreetingMassage()+" "+user_name;
+        greeting_textView.setText(greeting_msg);
+
+    }
+
+    String getGreetingMassage(){
+        Calendar c = Calendar.getInstance();
+        int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
+        if(timeOfDay >= 0 && timeOfDay < 12){
+            return "Good Morning";
+        }else if(timeOfDay >= 12 && timeOfDay < 16){
+            return "Good Afternoon";
+        }else if(timeOfDay >= 16 && timeOfDay < 21){
+            return "Good Evening";
+        }else if(timeOfDay >= 21 && timeOfDay < 24){
+            return "Good Night";
+        }
+        return null;
     }
 }

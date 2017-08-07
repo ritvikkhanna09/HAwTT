6ad2f8d9aced8550cc95144d41ef0789acf9bc24
Ritvik Khanna <ritvikkhanna09@gmail.com>
2017-08-07 14:44:01 +0530

    adding intent#2

--- App/HAwTT/app/src/main/java/ritvikkhanna/hawtt/IntroActivity.java
+++ App/HAwTT/app/src/main/java/ritvikkhanna/hawtt/IntroActivity.java
@@ -3,6 +3,7 @@ package ritvikkhanna.hawtt;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +17,7 @@ public class IntroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_intro);
         b=(Button)findViewById(R.id.button_gs);
         edt=(EditText)findViewById(R.id.edit_field);
+        edt.setFilters(new InputFilter[] {new InputFilter.AllCaps()});
         b.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
